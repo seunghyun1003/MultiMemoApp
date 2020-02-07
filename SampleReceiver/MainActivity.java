b3aa1049211d5e131196e3c87fef245e8acfa3d1
seunghyun1003 <58840682+seunghyun1003@users.noreply.github.com>
2020-02-07 15:52:13 +0900

    Create MainActivity.java

---
+++ SampleReceiver/MainActivity.java
@@ -0,0 +1,20 @@
+package org.androidtown.samplereceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
