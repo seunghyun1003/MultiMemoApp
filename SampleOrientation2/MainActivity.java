4101a5a5b215519b15f3638dab01623a1148279f
seunghyun1003 <58840682+seunghyun1003@users.noreply.github.com>
2020-02-10 13:36:59 +0900

    Create MainActivity.java

---
+++ SampleOrientation2/MainActivity.java
@@ -0,0 +1,29 @@
+package org.androidtown.sampleorientation2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+
+        if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){ //가로방향 전환 시 처리
+            showToast("방향 : ORIENTATION_LANDSCAPE");
+        } else if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){ //세로방향 전환 시 처리
+            showToast("방향 : ORIENTATION_PORTRAIT");
+        }
+    }
+    private void showToast(String data) {
+        Toast.makeText(this, data, Toast.LENGTH_LONG).show();
+    }
+}
